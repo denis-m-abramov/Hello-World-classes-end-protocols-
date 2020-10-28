3c1b59761fded4ccc7d26e601324cb7ff9c0465b
Denis Abramov <denis.m.abramov@mail.ru>
2020-10-29 00:22:42 +0300

    Add Hello World

---
+++ Hello World (classes end protocols).playground/Contents.swift
@@ -0,0 +1,64 @@
+class Player {
+    let name: String
+    let game: String
+    let age: Int
+    
+    var playerInfo: String {
+        let playerInfo = """
+        Player name is \(name).
+        He plays \(game).
+        He is \(age) years old.
+        """
+        return playerInfo
+    }
+    
+    init(name: String, game: String, age: Int) {
+        self.name = name
+        self.game = game
+        self.age = age
+    }
+}
+
+protocol PolitePlayer {
+    var friends: [Player] { get }
+    var haveAFriend: Bool { get }
+    
+    func smile()
+    func apologize() -> String
+}
+
+class ProfessionalPlayer: Player {
+    let experience: Int
+    let retirementAge: Int
+    init(name: String, game: String, age: Int, experience: Int, retirementAge: Int) {
+        self.experience = experience
+        self.retirementAge = retirementAge
+        super.init(name: name, game: game, age: age)
+    }
+}
+
+extension ProfessionalPlayer: PolitePlayer {
+    var friends: [Player] {
+        return [Player(name: "Alex", game: "Football", age: 19)]
+    }
+    
+    var haveAFriend: Bool {
+        return !friends.isEmpty
+    }
+    
+    func smile() {
+        print("Let's smile together")
+    }
+    
+    func apologize() -> String {
+        return "Sorry, friend!"
+    }
+}
+
+let player = Player(name: "Alexey", game: "Football", age: 21)
+print(player.playerInfo)
+
+let proPlayer = ProfessionalPlayer(name: "Misha", game: "Tennis", age: 24, experience: 5, retirementAge: 35)
+print(proPlayer.playerInfo)
+
+proPlayer.apologize()
